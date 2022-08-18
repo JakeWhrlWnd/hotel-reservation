a994776be9b3b17ac305a0689f2d4153692ea23e
JakeWhrlWnd <justjamescreative@outlook.com>
2022-08-18 01:43:17 -0400

    Updated the IRoom interface with modifiers for the methods. Updated the Room class. Added variables, a constructor, and overrides. Updated the FreeRoom and Customer Classes.

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,4 +1,49 @@
+/** Creates the Customer class. Defines the first name, last name, and email.
+ *  Uses a Regex to test for a valid email.
+ *
+ * @JamesNorris
+ *
+ */
 package model;
 
+import java.util.regex.Pattern;
+
 public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private String emailRegex = "^(.+)@(.+).(.+)$";
+    private Pattern emailPattern = Pattern.compile(emailRegex);
+
+    public Customer(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/model/FreeRoom.java
+++ src/model/FreeRoom.java
@@ -1,4 +1,18 @@
+/** This creates the FreeRoom class. It extends the Room class and overrides the price.
+ *
+ * @JamesNorris
+ *
+ */
 package model;
 
-public class FreeRoom {
+public class FreeRoom extends Room {
+
+    public FreeRoom(String roomNumber, Double price, RoomType enumeration) {
+        super(roomNumber, 0.0, enumeration);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
