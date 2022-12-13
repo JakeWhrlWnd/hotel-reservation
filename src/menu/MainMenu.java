abf132a07fa038e14800e3e42e869181c6eea730
JakeWhrlWnd <justjamescreative@outlook.com>
2022-12-13 01:31:02 -0500

    Adjusted and Updated

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -1,6 +1,9 @@
 package menu;
 
 import api.HotelResource;
+import api.AdminResource;
+import com.sun.net.httpserver.Authenticator;
+import model.Customer;
 import model.IRoom;
 import model.Reservation;
 
@@ -11,7 +14,6 @@ import java.time.format.DateTimeParseException;
 import java.util.*;
 
 public class MainMenu {
-    private static final HotelResource hotelResource = HotelResource.getInstance();
     private static final Scanner scanner = new Scanner(System.in);
     public static void showMainMenu() {
         System.out.println(mainMenuTxt);
@@ -47,6 +49,53 @@ public class MainMenu {
     }
 
     private static void createAnAccount() {
+        String email = "";
+        String firstName = "";
+        String lastName = "";
+
+        do {
+            // Validate First Name
+            System.out.println("""
+                Please, enter your First name:
+                (Must begin with a capital letter)
+                """);
+            try {
+                firstName = scanner.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Name must begin with a capital letter.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Name must consist of letters.");
+            }
+
+            // Validate Last Name
+            System.out.println("""
+                Please, enter your Last name:
+                (Must begin with a capital letter)
+                """);
+            try {
+                lastName = scanner.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Name must begin with a capital letter.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Name must consist of letters.");
+            }
+
+            // Validate Email
+            System.out.println("""
+                Please, enter your Email:
+                (Format must be - name@domain.com
+                """);
+            try {
+                email = scanner.nextLine();
+                HotelResource.createACustomer(firstName, lastName, email);
+                flag = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Email format must be - name@domain.com");
+            }
+        } while (!flag);
+
+        setCustomer(HotelResource.getCustomer(email));
+        System.out.println("Account creation successful. Welcome " + getCustomer().getFirstName() + "!");
     }
 
     public static void takeABreak() {
@@ -57,6 +106,15 @@ public class MainMenu {
         }
     }
     protected static boolean flag = true;
+
+    private static Customer customer;
+
+    public static void setCustomer(Customer customer) {
+        MainMenu.customer = customer;
+    }
+    public staic Customer getCustomer() {
+        return customer;
+    }
 
     protected static final String mainMenuTxt = """
             Welcome to the Hotel Reservation Application
